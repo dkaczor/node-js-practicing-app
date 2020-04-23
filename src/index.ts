630777581d29592a16cdcb3b0b038563bfd7d263
dariuszkaczor1990@gmail.com <dariuszkaczor1990@gmail.com>
2020-04-23 19:47:26 +0200

    add demon in app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express = require('express');
-import {variable} from './controllers/controller'
+import * as controllers from './controllers/controller'
 
 const app: express.Application = express();
 
 app.get('/', function (req, res) {
-  res.send(`Hello world and number ${variable}`);
+  res.send(`Hello world  and number ${controllers.variable}`);
 });
 
 app.listen(3000, function () {
